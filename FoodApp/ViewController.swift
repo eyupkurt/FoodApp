114a90678538f17c5b4623935860b64a9b18348b
Eyup Beycan Kurt <eyupbkurt@gmail.com>
2023-10-26 16:11:33 +0300

    Initial Commit

---
+++ FoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by Eyüp Kurt on 26.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
